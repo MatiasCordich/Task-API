b227e4bcf829345e8ef9e0fc510c5a9c5cbdea9b
MatiasCordich <m.scordich@gmail.com>
2022-09-25 15:23:07 -0300

    swagger added

--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router()
 
 const taskController = require('../controllers/TaskController')
 
+
+
 // Obtener todas las tareas
 
 router.get('/', taskController.getAllTasks)
@@ -13,6 +15,45 @@ router.get('/', taskController.getAllTasks)
 router.get('/:id', taskController.getOneTask)
 
 // Crear una tarea
+
+/**
+ * @swagger
+ * /api/v1/tasks:
+ *     post:
+ *       tags: 
+ *          - Post Task
+ *       summary: "Crear una nueva tarea"
+ *       description: Este endpoint me permite crear una nueva tarea para despues agregarla a la BD
+ *       requestBody:
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: "#/components/schemas/Task"
+ *       responses:
+ *          '200':
+ *           description: OK
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Task'
+ *          '400':
+ *           description: Error! There are empty fields
+ *           content:
+ *             application/json:
+ *               schema:
+ *                  type: object
+ *                  properties:
+ *                    status:
+ *                      type: string
+ *                      example: FAILED
+ *                    data:
+ *                      type: object
+ *                      properties:
+ *                        error:
+ *                          type: string
+ *                          example: "There are empty fields"
+ */
+
 
 router.post('/', taskController.createNewTask)
 
